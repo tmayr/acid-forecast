e5fe3ce4a0d3f673dc9e93ae1a2e659646e1baf7
Tomas Mayr <tmayr@tmayr.com>
2015-01-28 01:22:48 -0300

    added main css styling, using weather-icon fonts and mapped some forecast-io values to the appropriate contents added brocoli-static-compiler to import certain bower files "manually" using bower added weather-icons to bower and broccoli

--- Brocfile.js
+++ Brocfile.js
@@ -1,5 +1,19 @@
 var compileSass = require('broccoli-sass');
 var mergeTrees = require('broccoli-merge-trees');
+var findBowerTrees = require('broccoli-bower');
+var pickFiles = require('broccoli-static-compiler');
+
+var extraAssets = pickFiles('bower_components/weather-icons', {
+     srcDir: '/',
+     files: [
+        '**/*.woff',
+        '**/*.eot',
+        '**/*.svg',
+        '**/*.ttf',
+        '**/*.otf',
+        '**/weather-icons.min.css'],
+     destDir: '/assets'
+ });
 
 var appCss = compileSass(
     ['assets/scss'],
@@ -8,4 +22,4 @@ var appCss = compileSass(
     { outputStyle: 'compressed'}
 );
 
-module.exports = mergeTrees([appCss]);
+module.exports = mergeTrees([appCss, extraAssets]);
