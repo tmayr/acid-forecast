251cf842e935701cca9668180385716fb0d31415
Tomas Mayr <tmayr@tmayr.com>
2015-01-27 23:07:27 -0300

    first kinda working version, slugs will log to the console the output of forecast.io response, also exposing the key, will reset later

--- app.js
+++ app.js
@@ -1,16 +1,58 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var broccoli_middlware = require('broccoli-middleware');
+var https = require('https');
 
 var app = express();
+
+var forecast_uri = 'https://api.forecast.io/forecast/2fd1e5e0ed7da1df98ab7a244ea3e9fc'
+
+var cities = {
+    'new-york': {
+        'lat': '40.7142700',
+        'lng': '-74.0059700'
+    },
+    'buenos-aires': {
+        'lat': '-34.6131500',
+        'lng': '-58.3772300'
+    },
+    'lima': {
+        'lat': '-12.0431800',
+        'lng': '-77.0282400'
+    },
+    'santiago': {
+        'lat': '-33.4569400',
+        'lng': '-70.6482700'
+    },
+    'toronto': {
+        'lat': '43.7001100',
+        'lng': '-79.4163000'
+    }
+}
+
+function build_forecast_city_uri(city){
+    return forecast_uri + '/' + cities[city].lat + ',' + cities[city].lng;
+}
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res) {
-  res.render("index");
-});
+app.get('/:city', function(req, res) {
+  var city = req.params.city;
+  var city_uri = build_forecast_city_uri(city);
 
+  https.get(city_uri, function(response){
+    var body = '';
+    response.on('data', function(d){
+        body += d;
+    });
+
+    response.on('end', function(){
+        console.log(JSON.parse(body))
+        res.render("index");
+    })
+  });
+});
 
 // using the middleware as an assets pipeline, pretty cool
 app.use(broccoli_middlware);
