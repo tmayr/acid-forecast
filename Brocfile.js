67bbf7e899c6799af1d75b7b3f5cbd35fb399004
Tomas Mayr <tmayr@tmayr.com>
2015-01-27 22:32:50 -0300

    added normalize and app.css, broccoli now compiling css

--- Brocfile.js
+++ Brocfile.js
@@ -4,7 +4,8 @@ var mergeTrees = require('broccoli-merge-trees');
 var appCss = compileSass(
     ['assets/scss'],
     '/app.scss',
-    'assets/css/app.css'
+    'assets/css/app.css',
+    { outputStyle: 'compressed'}
 );
 
 module.exports = mergeTrees([appCss]);
