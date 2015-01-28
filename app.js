ea589612bb604a0b1e97047ae6e0628216264205
Tomas Mayr <tmayr@tmayr.com>
2015-01-27 21:33:36 -0300

    added main project structure/files

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res) {
+  res.render("index");
+});
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: err
+    });
+});
+
+app.listen(3000);
